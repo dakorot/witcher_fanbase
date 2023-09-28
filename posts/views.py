d0836fc1c8fe24c629ab379ee950f9e1a3e123fb
dakorot <koropenko.daria@gmail.com>
2023-09-28 14:00:14 +0200

    beta top and latest

--- posts/views.py
+++ posts/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 def home(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
+
+def top(request):
+    template = loader.get_template('top.html')
+    return HttpResponse(template.render())
+
+def latest(request):
+    template = loader.get_template('latest.html')
+    return HttpResponse(template.render())
